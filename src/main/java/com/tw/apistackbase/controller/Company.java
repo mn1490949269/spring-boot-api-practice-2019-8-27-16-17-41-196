88d28121d41454dcd5f9c16b8b9724b5d384be96
mn1490949269 <1490949269@qq.com>
2019-08-28 21:38:14 +0800

    新建Company实体类和Employee实体类

---
+++ src/main/java/com/tw/apistackbase/controller/Company.java
@@ -0,0 +1,35 @@
+package com.tw.apistackbase.controller;
+
+import java.util.List;
+
+public class Company {
+	private String companyName;
+	private int employeesNumber;
+	private List<Employee> employees;
+	public Company(String companyName, int employeesNumber, List<Employee> employees) {
+		super();
+		this.companyName = companyName;
+		this.employeesNumber = employeesNumber;
+		this.employees = employees;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public int getEmployeesNumber() {
+		return employeesNumber;
+	}
+	public void setEmployeesNumber(int employeesNumber) {
+		this.employeesNumber = employeesNumber;
+	}
+	public List<Employee> getEmployees() {
+		return employees;
+	}
+	public void setEmployees(List<Employee> employees) {
+		this.employees = employees;
+	}
+	
+
+}
